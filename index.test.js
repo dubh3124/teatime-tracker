ee2038684c60456e1b2d3eb3d8158c8c3f275ff8
Cognition Sandbox <sandbox@cognition.local>
2026-05-05 06:48:48 +0000

    Add failing test for logBrew

--- index.test.js
+++ index.test.js
@@ -1,18 +1,9 @@
-const { sum, subtract, multiply } = require('./index');
+const { logBrew } = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('subtracts 5 - 2 to equal 3', () => {
-  expect(subtract(5, 2)).toBe(3);
-});
-
-test('multiplies 2 * 3 to equal 6', () => {
-  expect(multiply(2, 3)).toBe(6);
-});
-
-test('division 6 / 2 to equal 3', () => {
-  const { divide } = require('./index');
-  expect(divide(6, 2)).toBe(3);
+test('logBrew(type, label) creates a brew entry with timestamp', () => {
+  const brew = logBrew('tea', 'Earl Grey');
+  expect(brew.type).toBe('tea');
+  expect(brew.label).toBe('Earl Grey');
+  expect(brew.timestamp).toBeDefined();
+  expect(new Date(brew.timestamp).getTime()).toBeLessThanOrEqual(Date.now());
 });
