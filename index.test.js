70b48a182e8e155593dcb3d79a5b0e1a9b0bda33
Cognition Sandbox <sandbox@cognition.local>
2026-05-05 05:45:44 +0000

    Add failing test for subtract function

--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ const { sum } = require('./index');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('subtracts 5 - 2 to equal 3', () => {
+  const { subtract } = require('./index');
+  expect(subtract(5, 2)).toBe(3);
+});
