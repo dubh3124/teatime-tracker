cd4717be6fe81ebcc7d3cea0602276de6286939c
Cognition Sandbox <sandbox@cognition.local>
2026-05-07 06:46:42 +0000

    Implement getDailySummary

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ const logBrew = (type, label) => {
 const getBrews = () => brews;
 
 const getDailySummary = () => {
-  return {};
+  const today = new Date().toISOString().split('T')[0];
+  const summary = {
+    tea: 0,
+    coffee: 0
+  };
+
+  brews.forEach(brew => {
+    const brewDate = brew.timestamp.split('T')[0];
+    if (brewDate === today) {
+      if (summary.hasOwnProperty(brew.type)) {
+        summary[brew.type]++;
+      }
+    }
+  });
+
+  return summary;
 };
 
 module.exports = { logBrew, getBrews, getDailySummary };
